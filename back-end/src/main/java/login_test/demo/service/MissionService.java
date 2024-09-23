511b3b8c08a9f91dd279e4655591a66cd3d49f8a
Chaemin <kcm9561@naver.com>
2024-09-23 10:28:35 +0900

    업적, 미션 메서드 수정

--- back-end/src/main/java/login_test/demo/service/MissionService.java
+++ back-end/src/main/java/login_test/demo/service/MissionService.java
@@ -4,23 +4,31 @@ import login_test.demo.dto.AchievementDto;
 import login_test.demo.dto.MissionDto;
 import login_test.demo.model.Achievement;
 import login_test.demo.model.Mission;
+import login_test.demo.model.User;
 import login_test.demo.repository.AchievementRepository;
 import login_test.demo.repository.MissionRepository;
+import login_test.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MissionService {
+    private final UserRepository userRepository;
     private final MissionRepository missionRepository;
     private final AchievementRepository achievementRepository;
 
     // 미션 추가
     public void addMission(MissionDto missionDto) {
+
+        User user = userRepository.findById(missionDto.getUser_id())
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
         Mission mission = Mission.builder()
                 .title(missionDto.getTitle())
                 .content(missionDto.getContent())
                 .missionReward(missionDto.getMissionReward())
+                .user(user)
                 .build();
 
         missionRepository.save(mission);
@@ -29,10 +37,15 @@ public class MissionService {
 
     // 업적 추가
     public void addAchievement(AchievementDto achievementDto) {
+
+        User user = userRepository.findById(achievementDto.getUser_id())
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
         Achievement achievement = Achievement.builder()
                 .title(achievementDto.getTitle())
                 .content(achievementDto.getContent())
                 .achievementReward(achievementDto.getAchievementReward())
+                .user(user)
                 .build();
 
         achievementRepository.save(achievement);
