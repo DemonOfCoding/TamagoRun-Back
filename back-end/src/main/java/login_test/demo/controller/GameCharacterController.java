177030166f8f1082ae17e5436949ee63cd77815c
Chaemin <kcm9561@naver.com>
2024-11-06 20:19:36 +0900

    추가 EvolutionDto.java 수정 GameCharacterController.java ,GameCharacter.java, AchievementService.java, GameCharacterService.java, MissionService.java

--- back-end/src/main/java/login_test/demo/controller/GameCharacterController.java
+++ back-end/src/main/java/login_test/demo/controller/GameCharacterController.java
@@ -1,8 +1,11 @@
 package login_test.demo.controller;
 
+import login_test.demo.dto.EvolutionDto;
 import login_test.demo.dto.GameCharacterDto;
 import login_test.demo.dto.SessionDto;
+import login_test.demo.model.GameCharacter;
 import login_test.demo.model.User;
+import login_test.demo.repository.GameCharacterRepository;
 import login_test.demo.repository.UserRepository;
 import login_test.demo.service.GameCharacterService;
 import login_test.demo.service.RedisUtil;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/character")
 public class GameCharacterController {
+    private final GameCharacterRepository gameCharacterRepository;
     private final GameCharacterService gameCharacterService;
     private final UserRepository userRepository;
     private final RedisUtil redisUtil;
@@ -54,16 +58,22 @@ public class GameCharacterController {
     }
 
     // 캐릭터 진화
-//    @PostMapping("/evolutionCharacter")
-//    public ResponseEntity<String> evolutionCharacter(@RequestBody GameCharacterDto gameCharacterDto) {
-//        String loginId = redisUtil.getData(gameCharacterDto.getSessionId());
-//        User user = userRepository.findByLoginId(loginId);
-//        int temp1;
-//        int temp2;
-//
-//        if (loginId == null || user == null) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 사용자를 찾을 수 없습니다.");
-//        }
-//
-//    }
+    @PostMapping("/evolutionCharacter")
+    public ResponseEntity<String> evolutionCharacter(@RequestBody GameCharacterDto gameCharacterDto) {
+        String loginId = redisUtil.getData(gameCharacterDto.getSessionId());
+        User user = userRepository.findByLoginId(loginId);
+        GameCharacter gameCharacter = gameCharacterRepository.findByUserId(user.getId());
+
+        if (loginId == null || user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 사용자를 찾을 수 없습니다.");
+        }
+
+        EvolutionDto evolutionDto = gameCharacterService.evolutionCharacter(user.getId());
+
+        // 캐릭터가 최대 레벨일 경우
+        if (gameCharacter.getEvolutionLevel() == 3)
+            return ResponseEntity.ok("캐릭터가 최대 레벨입니다.\n" + evolutionDto);
+
+        return ResponseEntity.ok("캐릭터가 진화하였습니다.\n" +evolutionDto);
+    }
 }
--- back-end/src/main/java/login_test/demo/model/GameCharacter.java
+++ back-end/src/main/java/login_test/demo/model/GameCharacter.java
@@ -21,11 +21,13 @@ public class GameCharacter {
 
     // 캐릭터 종족
     private int species;
-
     // 캐릭터 분류
     private int kindOfCharacter;
     // 캐릭터 단계
     private int evolutionLevel;
+    private boolean evolutionFlag0;
+    private boolean evolutionFlag1;
+    private boolean evolutionFlag2;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
